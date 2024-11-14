36c9c60c043fb0ca325ffb9c3cb44ec09f7c4b8c
HyphenPixel <hyphenpixel@protonmail.com>
2024-11-14 06:40:28 -0500

    unimportant

--- src/main.c
+++ src/main.c
@@ -2,23 +2,18 @@
 #include <stdio.h>
 #include <display.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     srand((uint8_t)time(NULL));
     int cycles = 10;
-    Chip8 chip8 = {0};
-    memset(chip8.vram, 0, sizeof(chip8.vram));
-    chip8.ram[0] = 0xA0;
-    chip8.ram[1] = 0x0F;
-    chip8.ram[2] = 0xD0;
-    chip8.ram[3] = 0x08;
-    chip8.ram[0xF] =    0b00100100;
-    chip8.ram[010] =    0b00100100;
-    chip8.ram[0x11] =   0b10000001;
-    chip8.ram[0x12] =   0b01111110;
+    Chip8 chip8 = initChip8();
+    
+    loadRom(&chip8, "program.c8");
 
     cycle_cpu(&chip8, cycles);
 
     Canvas c = initCanvas(WHITE, BLACK);
+    for (int i = 0x200; i < 0x220; ++i)
+        printf("0x%04x : 0x%04x\n", i, chip8.ram[i]);
     while (!WindowShouldClose())
         draw(&c, &chip8);
     cleanupCanvas(&c);
