a830a26ed13838348da112c01821972917cf4604
HyphenPixel <hyphenpixel@protonmail.com>
2024-11-13 20:02:13 -0500

    unimportant

--- src/main.c
+++ src/main.c
@@ -2,21 +2,17 @@
 #include <stdio.h>
 
 int main() {
-    int cycles = 10;
+    int cycles = 5;
     Chip8 chip8;
     chip8.ram[0] = 0x60;
-    chip8.ram[1] = 0xFF;
-    chip8.ram[2] = 0x30;
-    chip8.ram[3] = 0xFF;
-    chip8.ram[4] = 0xAA;
-    chip8.ram[5] = 0xCC;
-    chip8.ram[6] = 0x61;
-    chip8.ram[7] = 0x9A;
+    chip8.ram[1] = 0xFA;
+    chip8.ram[2] = 0x70;
+    chip8.ram[3] = 0x01;
 
     cycle_cpu(&chip8, cycles);
 
-    printf("\nV0: 0x%04x\n", chip8.v[0]);
-    printf("V1: 0x%04x\n", chip8.v[1]);
+    printf("\nV0: 0x%04X\n", chip8.v[0]);
+    printf("V1: 0x%04X\n", chip8.v[1]);
 
     return 0;
 }
