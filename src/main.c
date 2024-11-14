aef104a7e67b803768f5e8ded6ae607560722c1f
HyphenPixel <hyphenpixel@protonmail.com>
2024-11-14 05:44:15 -0500

    unimportant

--- src/main.c
+++ src/main.c
@@ -6,22 +6,19 @@ int main() {
     srand((uint8_t)time(NULL));
     int cycles = 10;
     Chip8 chip8 = {0};
-    /*chip8.ram[0] = 0x70;
-    chip8.ram[1] = 0x01;
-    chip8.ram[2] = 0xB1;
-    chip8.ram[3] = 0x00;
-    chip8.ram[0x101] = 0x70;
-    chip8.ram[0x102] = 0xFF;
-    //chip8.ram[4] = 0x80;
-    //chip8.ram[5] = 0x14;
+    memset(chip8.vram, 0, sizeof(chip8.vram));
+    chip8.ram[0] = 0xA0;
+    chip8.ram[1] = 0x0F;
+    chip8.ram[2] = 0xD0;
+    chip8.ram[3] = 0x08;
+    chip8.ram[0xF] =    0b00100100;
+    chip8.ram[010] =    0b00100100;
+    chip8.ram[0x11] =   0b10000001;
+    chip8.ram[0x12] =   0b01111110;
 
     cycle_cpu(&chip8, cycles);
 
-    printf("\nV0: 0x%03X\n", chip8.v[0]);
-    printf("V1: 0x%03X\n", chip8.v[1]);
-    printf("VF: 0x%03X\n", chip8.v[0xF]);
-    printf("I: 0x%03X\n", chip8.i);*/
-    Canvas c = initCanvas(ORANGE, WHITE);
+    Canvas c = initCanvas(WHITE, BLACK);
     while (!WindowShouldClose())
         draw(&c, &chip8);
     cleanupCanvas(&c);
