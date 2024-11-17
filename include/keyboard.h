1f59d25db43a2084a33d790f36dc773fa7e15cff
HyphenPixel <hyphenpixel@protonmail.com>
2024-11-17 10:05:24 -0500

    fixed keyboard layout

--- include/keyboard.h
+++ include/keyboard.h
@@ -2,10 +2,10 @@
 #include <raylib.h>
 
 static const int keyMap[16] = {
-    KEY_ONE, KEY_TWO, KEY_THREE, KEY_FOUR,  // 1, 2, 3, C
-    KEY_Q, KEY_W, KEY_E, KEY_R,             // 4, 5, 6, D
-    KEY_A, KEY_S, KEY_D, KEY_F,             // 7, 8, 9, E
-    KEY_Z, KEY_X, KEY_C, KEY_V              // A, 0, B, F
+    KEY_X, KEY_ONE, KEY_TWO, KEY_THREE,
+    KEY_Q, KEY_W, KEY_E, KEY_A, 
+    KEY_S, KEY_D, KEY_Z, KEY_C, 
+    KEY_FOUR, KEY_R, KEY_F, KEY_V
 };
 
 int getKey(void);
