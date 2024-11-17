9d259c2186f9f13073d15739d013d15d43aebbd8
HyphenPixel <hyphenpixel@protonmail.com>
2024-11-17 10:48:00 -0500

    added actions for cleaning files and setting the compiler

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,42 @@
 ---@diagnostic disable: undefined-global, undefined-field
+newaction {
+    trigger = "clean",
+    description = "Remove all build files in obj folder",
+    execute = function ()
+        print("Cleaning build files...")
+        os.rmdir("obj")
+        print("Clean complete!")
+    end
+}
+
+newaction {
+    trigger = "cc",
+    description = "Set the compiler to use (gcc or clang)",
+    validargs = { "gcc", "clang" },
+    execute = function ()
+        local compiler = _ARGS[1]
+        if not compiler then
+            print("Usage: premake5 cc <gcc|clang>")
+            return
+        end
+
+        if compiler ~= "gcc" and compiler ~= "clang" then
+            print("Invalid compiler. Choose 'gcc' or 'clang'.")
+            return
+        end
+
+        local config_file = "compiler.cfg"
+        local file = io.open(config_file, "w")
+        if file then
+            file:write(compiler)
+            file:close()
+            print("Compiler set to: " .. compiler)
+        else
+            print("Error: Unable to write compiler configuration.")
+        end
+    end
+}
+
 workspace "Chip8"
     configurations { "Debug", "Release" }
 
@@ -6,6 +44,16 @@ project "Chip8"
     kind "ConsoleApp"
     language "C"
     targetdir "output/%{cfg.buildcfg}"
+
+    local config_file = "compiler.cfg"
+    local compiler = "gcc" -- Default compiler
+    local file = io.open(config_file, "r")
+    if file then
+        compiler = file:read("*all")
+        file:close()
+    end
+
+    toolset(compiler)
 
     includedirs { "include" }
 
